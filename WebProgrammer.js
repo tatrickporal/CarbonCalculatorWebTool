eee37bd60587db1c6789062d0e763c3b577e38c0
Patrick Toral <tatrickporal@eduroam-169-233-219-167.ucsc.edu>
2017-09-29 10:50:43 -0700

    First

---
+++ WebProgrammer.js
@@ -0,0 +1,40 @@
+// These  updates the numbers just beneath the submit button
+// The first controls the plastic bottle numbers
+$(":input").on('keyup mouseup', function () {
+  var plasticNumber = $("#plasticNumber").val();
+  $("#plastics").text(plasticNumber);
+}).trigger('mouseup');
+
+// The second controls the paper cup numbers
+$(":input").on('keyup mouseup', function () {
+  var paperNumber = $("#paperNumber").val();
+  $("#papers").text(paperNumber);
+}).trigger('mouseup');
+
+//This function controls the output and is called when the submit button is pressed
+function submit() {
+  //User inputted numbers 
+  var plasticNumber = $('#plasticNumber').val();
+  var paperNumber = $('#paperNumber').val();
+  		//These next variables control the calculations this first block is weekly emissions
+    	var plasticEmission = 0.32;
+    	var paperEmission = .25;
+    	var plasticEmissionNumber = plasticEmission * plasticNumber;
+    	var paperEmissionNumber = paperEmission * paperNumber;
+    	//This is the total number combined with the numbers above
+    	var totalUnfixed = plasticEmissionNumber + paperEmissionNumber;
+    	var total = totalUnfixed.toFixed(2);
+    	//This block controlls the calculations for "What do these numbers mean?" section
+    	var carOutput = 0.91;
+    	var milesUnfixed  = total/.91;
+    	var miles = milesUnfixed.toFixed(2);
+    	var milesYearlyUnfixed = miles * 52;
+    	var milesYearly = milesYearlyUnfixed.toFixed(2); 
+  // This code block outputs all the data so it can be displayed on the browser
+  //Where to output is done by selecting the respective ID's
+  document.getElementById("output").innerHTML = "You produce a total of " + total + "lbs of CO2 weekly" ;
+  document.getElementById("individualInfo").innerHTML =  plasticEmissionNumber + "lbs of CO2 realeased from the creation of the plastic" + " +  " + paperEmissionNumber + "lbs of CO2 realeased from using paper cups = " + total + " total pounds of CO2 produced" ;
+  document.getElementById("info").innerHTML = "What do these number mean?" ;
+  document.getElementById("moreInfo").innerHTML = "To make these numbers more understandable:"  ;
+  document.getElementById("moreInfoTwo").innerHTML = " - "+ total + "lbs of CO2 is the same amount of CO2 released from driving " + miles + " miles in an average car, " + milesYearly + " miles yearly!";
+}   
