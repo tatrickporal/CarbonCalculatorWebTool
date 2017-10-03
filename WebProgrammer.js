7518fe3df67feb25b49d52d94725c1208fa548a3
Patrick Toral <tatrickporal@eduroam-169-233-253-164.ucsc.edu>
2017-10-03 10:22:21 -0700

    Second

--- WebProgrammer.js
+++ WebProgrammer.js
@@ -37,4 +37,9 @@ function submit() {
   document.getElementById("info").innerHTML = "What do these number mean?" ;
   document.getElementById("moreInfo").innerHTML = "To make these numbers more understandable:"  ;
   document.getElementById("moreInfoTwo").innerHTML = " - "+ total + "lbs of CO2 is the same amount of CO2 released from driving " + miles + " miles in an average car, " + milesYearly + " miles yearly!";
+  document.getElementById("citations").innerHTML = "Resrources and Articles"; 
+  document.getElementById("citationOne").innerHTML = "<li>Pacific Institute Bottled Water Fact Sheet</li>";
+  document.getElementById("citationTwo").innerHTML = "<li>EPA's Greenhouse Gases Equivalencies Calculator</li>";
+  document.getElementById("citationThree").innerHTML = "<li>Enviromental Research Letters: Energy Implications of Bottled Water</li>";
+  document.getElementById("citationFour").innerHTML = "<li>Disposable Coffee Cup Infographic</li>";
 }   
