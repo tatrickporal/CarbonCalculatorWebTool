0b7ce9fb295a683a0130d8d566b19c3f1eb0d653
Patrick Toral <tatrickporal@eduroam-169-233-227-186.ucsc.edu>
2017-10-10 10:37:56 -0700

    TimeStamp 10/9 10:37AM

--- WebProgrammer.js
+++ WebProgrammer.js
@@ -11,18 +11,27 @@ $(":input").on('keyup mouseup', function () {
   $("#papers").text(paperNumber);
 }).trigger('mouseup');
 
+// The third controls the paper towel numbers
+$(":input").on('keyup mouseup', function () {
+  var paperTowelNumber = $("#paperTowelNumber").val();
+  $("#paperTowels").text(paperTowelNumber);
+}).trigger('mouseup');
+
 //This function controls the output and is called when the submit button is pressed
 function submit() {
   //User inputted numbers 
   var plasticNumber = $('#plasticNumber').val();
   var paperNumber = $('#paperNumber').val();
+  var paperTowelNumber = $('#paperTowelNumber').val();
   		//These next variables control the calculations this first block is weekly emissions
     	var plasticEmission = 0.32;
     	var paperEmission = .25;
+      var towelEmission = 0.12;
     	var plasticEmissionNumber = plasticEmission * plasticNumber;
     	var paperEmissionNumber = paperEmission * paperNumber;
+      var towelEmissionNumber = (towelEmission * paperTowelNumber) * 7;
     	//This is the total number combined with the numbers above
-    	var totalUnfixed = plasticEmissionNumber + paperEmissionNumber;
+    	var totalUnfixed = plasticEmissionNumber + paperEmissionNumber + towelEmissionNumber;
     	var total = totalUnfixed.toFixed(2);
     	//This block controlls the calculations for "What do these numbers mean?" section
     	var carOutput = 0.91;
@@ -36,9 +45,9 @@ function submit() {
 
   // This code block outputs all the data so it can be displayed on the browser
   //Where to output is done by selecting the respective ID's
-  document.getElementById("output").innerHTML = "You produce a total of " + total + "lbs of CO2 weekly, <span style='font-size:20px;'>("+ plasticEmissionNumber+"lbs from Plastic and "+paperEmissionNumber+"lbs from paper)</span> " ;
+  document.getElementById("output").innerHTML = "You produce a total of " + total + "lbs of CO2 weekly, <span style='font-size:20px;'>("+ plasticEmissionNumber+"lbs from plastic bottles, "+paperEmissionNumber+"lbs from paper cups, "+towelEmissionNumber+"lbs from paper towels )</span> " ;
  
-  document.getElementById("info").innerHTML = "<a href='#' >What do these number mean?</a>" ; 
+  document.getElementById("info").innerHTML = "<a target='_blank' href='https://www.epa.gov/climate-indicators/climate-change-indicators-us-greenhouse-gas-emissions' >What do these number mean?</a>" ; 
   document.getElementById("moreInfoTwo").innerHTML = " - <b>"+ total + "lbs</b> of CO2 is the same amount of CO2 released from driving <b>" + miles + "</b> miles in an average car, <b>" + milesYearly + "</b> miles yearly!";
   document.getElementById("moreInfoThree").innerHTML = " - With those yearly miles you would be taking the trip from here to the UC Santa Cruz Campus <b>" + trips + "</b> times";
 if(milesYearly > 20 && milesYearly < 30){
@@ -54,7 +63,7 @@ if(milesYearly > 100 && milesYearly < 140){
 document.getElementById("moreInfoSeven").innerHTML = " * or a trip up to <b>Downtown Sacramento!</b>";
 }
 if(milesYearly > 141){
-document.getElementById("moreInfoEight").innerHTML = " * or a trip accross statelines into <b>Nevada!</b>";
+document.getElementById("moreInfoEight").innerHTML = " * or a trip across statelines into <b>Nevada!</b>";
 }
 
   
@@ -64,4 +73,5 @@ document.getElementById("moreInfoEight").innerHTML = " * or a trip accross state
   document.getElementById("citationTwo").innerHTML = "<li>EPA's Greenhouse Gases Equivalencies Calculator</li>";
   document.getElementById("citationThree").innerHTML = "<li>Enviromental Research Letters: Energy Implications of Bottled Water</li>";
   document.getElementById("citationFour").innerHTML = "<li>Disposable Coffee Cup Infographic</li>";
+  document.getElementById("citationFive").innerHTML = "<li>Stanford Article on Paper Towel Emissions</li>";
 } 
